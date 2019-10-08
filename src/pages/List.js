3ef8df11984797f69d3fadfaf038b1ad1ac63a84
Júnior Theodoro <juninho_lpisrock@hotmail.com>
2019-10-07 22:50:19 -0300

    add app mobile omnistack 9.0

---
+++ src/pages/List.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Text, AsyncStorage } from 'react-native';
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return <Text>{techs}</Text>
+}
